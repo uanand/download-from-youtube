d0fb77db392c55864b9fb005d663af52b4142100
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-08-04 18:42:55 +0800

    Download mp4 and convert to mp3 successful.

--- download.py
+++ download.py
@@ -3,25 +3,41 @@ import pandas
 import ffmpeg
 import wget
 from pytube import YouTube
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3,APIC
+from mutagen.easyid3 import EasyID3
 
-df = pandas.read_excel('download.xlsx',names=['link','mode'])
+df = pandas.read_excel('download.xlsx',names=['link','mode','title','artist','album'])
 
-yt = YouTube('https://www.youtube.com/watch?v=jHNNMj5bNQw')
-# thumbnail = wget.download(yt.thumbnail_url)
-for stream in yt.streams:
-    if (stream.type=='audio'):
-        if (stream.mime_type=='audio/mp4' and stream.abr=='128kbps'):
-            downloadFileName = stream.default_filename
-            newFileName = 'audio'+'/'+downloadFileName.replace('.mp4','.mp3')
-            
-            print (downloadFileName)
-            stream.download()
-            
-            audio = ffmpeg.input(downloadFileName)
-            audio = ffmpeg.output(audio,newFileName)
-            ffmpeg.run(audio)
-            
-            
-            
-# for link,mode in df.values:
-    # print (link,mode)
+for link,mode,title,artist,album in df.values:
+    fileName = 'audio'+'/'+title+'.mp3'
+    if not(os.path.exists(fileName)):
+        yt = YouTube(link)
+        thumbnailFile = wget.download(yt.thumbnail_url)
+        
+        for stream in yt.streams:
+            if (stream.type=='audio'):
+                if (stream.mime_type=='audio/mp4' and stream.abr=='128kbps'):
+                    downloadFileName = stream.default_filename
+                    
+                    stream.download()
+                    
+                    # convert mp4 to mp3
+                    audio = ffmpeg.input(downloadFileName)
+                    audio = ffmpeg.output(audio,fileName)
+                    ffmpeg.run(audio)
+                    os.remove(downloadFileName)
+                    
+        # ADD COVER
+        audio = MP3(fileName,ID3=ID3)
+        audio.tags.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open(thumbnailFile,'rb').read()))
+        audio.save()
+        os.remove(thumbnailFile)
+        
+        # ADD TITLE AND ALBUM NAME
+        audio = EasyID3(fileName)
+        audio['title'] = title
+        audio['artist'] = artist
+        audio['album'] = album
+        audio.save()
+    
